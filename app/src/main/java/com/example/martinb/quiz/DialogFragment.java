da299a2c58bc1c23c084b501c7dd0767ea892791
Martin Bombara <martinb@lagash.com>
2018-03-21 15:17:56 -0300

    Added dialog fragment, listener and layouts

---
+++ app/src/main/java/com/example/martinb/quiz/DialogFragment.java
@@ -0,0 +1,111 @@
+package com.example.martinb.quiz;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DialogFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DialogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DialogFragment extends android.support.v4.app.DialogFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private onSelectedImage onSelectedImage;
+
+    private OnFragmentInteractionListener mListener;
+
+    public DialogFragment() {
+        // Required empty public constructor
+    }
+
+    @BindView(R.id.taphere)
+    Button continueButton;
+
+
+    // TODO: Rename and change types and number of parameters
+    public static DialogFragment newInstance() {
+        DialogFragment fragment = new DialogFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_dialog, container, false);
+        ButterKnife.bind(this,view);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSelectedImage.response("OK");
+                dismiss();
+            }
+        });
+        return view;
+    }
+
+    public void setListener(onSelectedImage onSelectedImage){
+        this.onSelectedImage = onSelectedImage;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/example/martinb/quiz/onSelectedImage.java
@@ -0,0 +1,10 @@
+package com.example.martinb.quiz;
+
+/**
+ * Created by martinb on 3/21/2018.
+ */
+
+public interface onSelectedImage {
+
+    void response (String response);
+}
