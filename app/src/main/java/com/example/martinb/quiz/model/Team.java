157c393769ce3ef2e9c05fc34f70bf3c4328ad5e
stdalessandro <stefano1dalessandro@gmail.com>
2018-03-27 23:07:27 -0300

    Added libs gson, etc. Added classes PlayerProfile from jsonconverter

---
+++ app/src/main/java/com/example/martinb/quiz/model/Team.java
@@ -0,0 +1,65 @@
+
+package com.example.martinb.quiz.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Team {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("country_code")
+    @Expose
+    private String countryCode;
+    @SerializedName("abbreviation")
+    @Expose
+    private String abbreviation;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+}
